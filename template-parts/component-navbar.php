6c38f0b8e8e03f6354bbb501962b20d4b353a5e0
vanderms <vanderms.84@gmail.com>
2021-09-13 20:09:46 -0300

    added sidebar to project

--- template-parts/component-navbar.php
+++ template-parts/component-navbar.php
@@ -1,17 +1,22 @@
 <nav class="pwcode-navbar">
     <?php the_custom_logo(); ?>
 
-    <?php if (has_nav_menu( 'navbar-center')) : ?>
-      <?php wp_nav_menu([
-        'theme_location' => 'navbar-center',
-        'container' => '',        
-    ]); ?>
-    <?php endif; ?>
+    <div class="pwcode-links">
+      
+      <?php if (has_nav_menu( 'navbar-center')) : ?>
+        <?php wp_nav_menu([
+          'theme_location' => 'navbar-center',
+          'container' => '',        
+      ]); ?>
+      <?php endif; ?>
+      
+      <?php if (has_nav_menu( 'navbar-right')) : ?>
+        <?php wp_nav_menu([
+          'theme_location' => 'navbar-right',
+          'container' => '',        
+      ]);?>    
+      <?php endif; ?>
+
+    </div>
     
-    <?php if (has_nav_menu( 'navbar-right')) : ?>
-      <?php wp_nav_menu([
-        'theme_location' => 'navbar-right',
-        'container' => '',        
-    ]);?>    
-    <?php endif; ?>
   </nav>
